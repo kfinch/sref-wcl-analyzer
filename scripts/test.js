3dfd592aa10ea1ec19d68d7a5415738866417b48
Kelton Finch <keltonfinch@gmail.com>
2017-04-16 08:05:25 -0400

    Logged report code

--- scripts/test.js
+++ scripts/test.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
 		
 		// get fights JSON
 		var reportCode = $("#report-input").value;
+		console.log("reportCode=" + reportCode);
 		var apiKey = "087783eb78c21061d028831d2344d118";
 		var url = "https://www.warcraftlogs.com/v1/report/fights/" + reportCode
 	  		+ "?api_key=" + apiKey;
