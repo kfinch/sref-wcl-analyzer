64c1af3f6c402f6a7f175bea388673b0932c426e
Kelton Finch <keltonfinch@gmail.com>
2017-04-16 07:29:03 -0400

    added console logging for debugging

--- scripts/test.js
+++ scripts/test.js
@@ -2,12 +2,17 @@ $(document).ready(function(){
     alert("jQuery works!");
 	
     $("#report-button").button().click(function(){
+		console.log("report button pressed");
+		
         // get fights JSON
 		var reportCode = $("#report-input:input");
 		var apiKey = "087783eb78c21061d028831d2344d118";
 		var url = "https://www.warcraftlogs.com/v1/report/fights/" + reportCode
 	  		+ "?api_key=" + apiKey;
 		var reportObj = $.getJson(url);
+		
+		console.log("fetching json from " + url);
+		console.log(JSON.stringify(reportObj));
 		
 		// test if we've got fights JSON
 		var fights = reportObj.fights;		
