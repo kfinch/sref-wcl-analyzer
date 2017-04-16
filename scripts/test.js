5013801b1d385c29c0212a7be94d0d8a3255bd57
Kelton Finch <keltonfinch@gmail.com>
2017-04-16 07:35:57 -0400

    Removed annoying alert on pageload

--- scripts/test.js
+++ scripts/test.js
@@ -1,22 +1,22 @@
 $(document).ready(function(){
-    alert("jQuery works!");
+	console.log("document ready!");
 	
-    $("#report-button").button().click(function(){
+	$("#report-button").button().click(function(){
 		console.log("report button pressed");
 		
-        // get fights JSON
+		// get fights JSON
 		var reportCode = $("#report-input:input");
 		var apiKey = "087783eb78c21061d028831d2344d118";
 		var url = "https://www.warcraftlogs.com/v1/report/fights/" + reportCode
 	  		+ "?api_key=" + apiKey;
+		console.log("fetching json from " + url);
 		var reportObj = $.getJSON(url);
 		
-		console.log("fetching json from " + url);
 		console.log(JSON.stringify(reportObj));
 		
 		// test if we've got fights JSON
 		var fights = reportObj.fights;		
 		$("#fight-div").append(fights[0].name);
-    });    
+	});    
 
 });
