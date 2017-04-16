1c5e7f5fe1625a36ff8cb051eafb16f442eb45e1
Kelton Finch <keltonfinch@gmail.com>
2017-04-16 07:17:42 -0400

    Replace pure JS test with jquery test for report fetch

--- scripts/test.js
+++ scripts/test.js
@@ -2,43 +2,16 @@ $(document).ready(function(){
     alert("jQuery works!");
 	
     $("#report-button").button().click(function(){
-        alert("button");
+        // get fights JSON
+		var reportCode = $("#report-input:input");
+		var apiKey = "087783eb78c21061d028831d2344d118";
+		var url = "https://www.warcraftlogs.com/v1/report/fights/" + reportCode
+	  		+ "?api_key=" + apiKey;
+		var reportObj = $.getJson(url);
+		
+		// test if we've got fights JSON
+		var fights = reportObj.fights;		
+		$("#fight-div").append(fights[0].name);
     });    
 
 });
-
-
-
-function getFights() {
-	// make menu
-	var menuDiv = document.getElementById('menu-div'); 
-	var fightList = document.createElement("select");
-	fightList.id = "fight-list";
-	menuDiv.appendChild(fightList);
-
-	// get fights JSON
-	var apiKey = "087783eb78c21061d028831d2344d118"
-	var url = "https://www.warcraftlogs.com/v1/report/fights/" + reportCode
-	  + "?api_key=" + apiKey
-	var reportObj = JSON.parse(getJson(url));
-
-	// test
-	var fightString = reportObj.fights;
-	menuDiv.innerHTML = fightString[0];
-
-	// populate menu with fight names
-	var fights = reportObj.fights;
-	for (var i = 0; i < fights.length; i++) {
-	  var option = document.createElement("option");
-	  option.value = fights[i].name;
-	  option.text = fights[i].name;
-	  fightList.appendChild(option);
-	}
-}
-
-function getJson(yourUrl) {
-	var Httpreq = new XMLHttpRequest(); // a new request
-	Httpreq.open("GET",yourUrl,false);
-	Httpreq.send(null);
-	return Httpreq.responseText;          
-}
